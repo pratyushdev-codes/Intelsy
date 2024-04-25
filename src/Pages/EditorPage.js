9a1b6edb2dc0e515716b8674326a88b17d13b1be
Pratyush Birole <birole.pratyush@gmail.com>
2024-04-25 08:57:48 +0530

    Wrapper Class component | Editor

--- src/Pages/EditorPage.js
+++ src/Pages/EditorPage.js
@@ -2,7 +2,42 @@ import React from 'react'
 
 function EditorPage() {
   return (
-    <div>EditorPage</div>
+    <div className='mainWrap'>
+      <div className='aside'>
+
+        <div className='asideInner'>
+
+          <div className="logo">
+
+            <img
+              className='logoImage'
+              src="../images/logo.png"
+              alt="Logo"
+            />
+
+            <h3>Connected Successfully, Start developing
+              
+</h3>
+
+
+
+
+          </div>
+
+
+
+
+        </div>
+
+
+
+      </div>
+
+
+
+      <div className='editorWrap'>Editor...</div>
+
+    </div>
   )
 }
 

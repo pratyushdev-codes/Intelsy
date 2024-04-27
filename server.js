74dc9a460db43e46f37396679260bc9b11d76d42
Pratyush Birole <birole.pratyush@gmail.com>
2024-04-27 16:31:59 +0530

    get all usernames from Websocket adapters

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const app = express();
 const http = require('http');
 const { Server } = require('socket.io');
+const ACTIONS = require ('./src/Action');
 
-const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const userSocketMap={};
+function getAllConnectedClients(roomId){
+    Array.from(io.socket.adapter.rooms.get(roomId));
+}
+
+
 
 io.on('connection', (socket) => {
     console.log('socket connected', socket.id);
+
+
+    socket.on(ACTIONS.JOIN , ({roomId, username})=>{
+        userSocketMap[socket.id]= username;
+        socket.join(roomId);
+        const clients = getAllConnectedClients(roomId);
+
+
+    })
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 server.listen(PORT, () => console.log(`Listening on Port ${PORT}`));

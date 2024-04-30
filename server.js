9eea98d87d2161ed964cb5dd4dbcbf4d537bcd4b
Pratyush Birole <birole.pratyush@gmail.com>
2024-04-30 11:02:36 +0530

    FIxed Web socket error

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
-const ACTIONS = require('./src/Action'); // Assuming this path is correct
-const { default: toast } = require('react-hot-toast');
+const ACTIONS = require('./src/Action');
+
 const server = http.createServer(app);
 const io = new Server(server);
-const userSocketMap = {};
 
+app.use(express.static('build'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const userSocketMap = {};
 function getAllConnectedClients(roomId) {
-    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
-        return {
-            socketId,
-            username: userSocketMap[socketId],
-        };
-    });
+    // Map
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+        (socketId) => {
+            return {
+                socketId,
+                username: userSocketMap[socketId],
+            };
+        }
+    );
 }
 
 io.on('connection', (socket) => {
@@ -24,34 +33,35 @@ io.on('connection', (socket) => {
         userSocketMap[socket.id] = username;
         socket.join(roomId);
         const clients = getAllConnectedClients(roomId);
-        clients.forEach((client) => {
-            if (client.username !== username) {
-                io.to(client.socketId).emit(ACTIONS.JOINED, {
-                    clients,
-                    username,
-                    socketId: socket.id,
-                });
-                toast.success(`${username} joined the playground`);
-            }
+        clients.forEach(({ socketId }) => {
+            io.to(socketId).emit(ACTIONS.JOINED, {
+                clients,
+                username,
+                socketId: socket.id,
+            });
         });
     });
 
-    socket.on(ACTIONS.CODE_ONCHANGE, ({roomId, code})=>{
-        io.to(roomId).emit(ACTIONS.CODE_CHANGE, {code});
+    socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+        socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
     });
 
-    socket.on('disconnect', () => {
-        socket.rooms.forEach((roomId) => {
-            socket.to(roomId).emit(ACTIONS.DISCONNECTED, {
+    socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+        io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+    });
+
+    socket.on('disconnecting', () => {
+        const rooms = [...socket.rooms];
+        rooms.forEach((roomId) => {
+            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
                 socketId: socket.id,
-                username: userSocketMap[socket.id]
+                username: userSocketMap[socket.id],
             });
         });
         delete userSocketMap[socket.id];
-        socket.leaveAll();
+        socket.leave();
     });
 });
 
 const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));

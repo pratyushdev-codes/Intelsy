d411e2934baa3bbf3c6bd0110b4008c44d3e10cf
Pratyush Birole <birole.pratyush@gmail.com>
2024-05-05 12:30:36 +0530

    Fixed Accesibility Bar and ITS FUNC

--- src/Components/AccessibilityBar.js
+++ src/Components/AccessibilityBar.js
@@ -9,35 +9,31 @@ const AccessibilityBar = () => {
   };
 
   const closeEditor = () => {
-    setIsEditorOpen(false);
+    setIsEditorOpen(false); 
   };
 
   return (
     <div style={{ zIndex: 9999, marginTop: "1rem", display: "flex", justifyContent: "center" }}>
-       <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ borderRadius: "30px", width: "50vh", height: "7vh", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)" }}>
+       <nav className="navbar navbar-expand-lg" style={{ borderRadius: "30px", width: "50vh", height: "7vh", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)", backgroundColor: "rgba(255, 255, 255, 0.2)" }}>
         <div className="container-fluid">
           <a className="navbar-brand" href="#" style={{color: "#036EFD", fontSize: "20px", fontWeight: "bold" }}>Accessibility <span style={{color:"#EC7A6F"}}>Bar</span></a>
           &nbsp;  &nbsp;  &nbsp;
           
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-
-            <li className="nav-item">
-                <a className="nav-link"><img src="https://iili.io/JgM3x0x.png"  style={{width:"45px", height:"40px"}}/></a>
-              </li>
               <li className="nav-item">
-                <a className="nav-link " aria-current="page" ><img src="https://iili.io/JgM3TJV.md.png"  style={{width:"45px", height:"40px"}}></img></a>
+                <a className="nav-link"><i className="fa-solid fa-camera-retro" style={{color: "#808080" , scale:"1.5"}} ></i></a>
               </li>
-             &nbsp;
+              &nbsp; &nbsp; 
+              <li className="nav-item">
+                <a className="nav-link"><i className="fa-solid fa-eraser" style={{color: "#808080" , scale:"1.5"}} ></i></a>
+              </li>
+             &nbsp; &nbsp;
               
               <li className="nav-item">
-                <a className="nav-link"><img src="https://iili.io/JgM3zUQ.png" onClick={openEditor} style={{width:"40px", height:"40px"}}/></a>
+                <a className="nav-link"><i className="fa-solid fa-pen-to-square" style={{color: "#808080" , scale:"1.5"}}  onClick={openEditor} ></i></a>
               </li>
-             
-              
             </ul>
-            
           </div>
         </div>
       </nav>

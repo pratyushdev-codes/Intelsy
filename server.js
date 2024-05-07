a9f681bd7e9e2c6fa3609cc6bc4821947db76006
Pratyush Birole <birole.pratyush@gmail.com>
2024-05-07 18:48:28 +0530

    Complier needs to be integrated

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ io.on('connection', (socket) => {
         
            
         });
-    });
+    }); 
 
     socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
         socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });

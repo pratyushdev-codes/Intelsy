5c33ba584e2b19c369b07ff12132e101cba15a94
Pratyush Birole <birole.pratyush@gmail.com>
2024-06-29 19:58:53 +0530

    Fixed compiler css | final fr now

--- src/Pages/EditorPage.js
+++ src/Pages/EditorPage.js
@@ -267,7 +267,7 @@ const generateAssist = async (code) => {
           </div>
         </div>
 
-        <div className='aside' style={{ boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" }}>
+        <div className='aside' style={{ boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.7), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" }}>
           <div className='asideInner'>
             <div className='logo'>
               <img
